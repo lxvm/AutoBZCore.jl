d56bb0c24a7ef465aa4fe9b3702b92608d2e58f7
lxvm <lorenzo@vanmunoz.com>
2023-02-15 19:15:50 -0500

    fix symmetry factors

--- src/iai_bz.jl
+++ src/iai_bz.jl
@@ -1,12 +1,11 @@
+"""
+    iterated_integration(f, bz::SymmetricBZ; kwargs...)
+"""
 function iterated_integration(f, bz::SymmetricBZ; kwargs...)
     kw = iterated_integration_kwargs(f, bz; kwargs...)
-    int, err = iterated_integration(f, bz.lims; kw...)
+    atol = kw.atol/nsyms(bz) # reduce absolute tolerance by symmetry factor
+    int, err = iterated_integration(f, bz.lims; kw..., atol=atol)
     symmetrize(f, bz, int, err)
 end
-function iterated_integration_kwargs(f, bz::SymmetricBZ; kwargs...)
-    kw = iterated_integration_kwargs(f, bz.lims; kwargs...)
-    (; kw..., atol=kw.atol/nsyms(bz)) # rescaling by symmetries
-end
-iterated_integral_type(f, bz::SymmetricBZ) = iterated_integral_type(f, bz.lims)
-iterated_inference(f, bz::SymmetricBZ) = iterated_inference(f, bz.lims)
-alloc_segbufs(f, bz::SymmetricBZ) = alloc_segbufs(f, bz.lims)
+iterated_integration_kwargs(f, bz::SymmetricBZ; kwargs...) =
+    iterated_integration_kwargs(f, bz.lims; kwargs...)
--- src/ptr_bz.jl
+++ src/ptr_bz.jl
@@ -1,15 +1,19 @@
 """
     symptr(f, bz::SymmetricBZ; kwargs...)
 """
-symptr(f, bz::SymmetricBZ; kwargs...) =
-    symptr(f, bz.B, bz.syms; kwargs...)
+function symptr(f, bz::SymmetricBZ; kwargs...)
+    int, rules = symptr(f, bz.B, bz.syms; kwargs...)
+    symmetrize(f, bz, int), rules
+end
 symptr_kwargs(f, bz::SymmetricBZ; kwargs...) =
     symptr_kwargs(f, bz.B, bz.syms; kwargs...)
 
 """
     autosymptr(f, bz::SymmetricBZ; kwargs...)
 """
-autosymptr(f, bz::SymmetricBZ; kwargs...) =
-    autosymptr(f, bz.B, bz.syms; kwargs...)
+function autosymptr(f, bz::SymmetricBZ; kwargs...)
+    int, err, numevals, rules = autosymptr(f, bz.B, bz.syms; kwargs...)
+    symmetrize(f, bz, int, err)..., numevals, rules
+end
 autosymptr_kwargs(f, bz::SymmetricBZ; kwargs...) =
     autosymptr_kwargs(f, bz.B, bz.syms; kwargs...)
