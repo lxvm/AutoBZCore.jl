096d1ad1adff622579288f7ae64ff198b94a29e1
lxvm <lorenzo@vanmunoz.com>
2023-03-05 23:56:32 -0500

    add frame to example plots

--- aps_example/aps_example.jl
+++ aps_example/aps_example.jl
@@ -17,7 +17,7 @@ alg = IAI(; segbufs=AutoBZCore.alloc_segbufs(Float64,Float64,Float64,3))
 dos_solver = IntegralSolver(integrand, bz, alg; abstol=1e-3)
 dos = hchebinterp(dos_solver, 10, 15; atol=1e-2)
 
-plot(; xguide="ω (eV)", yguide="SVO DOS (eV⁻¹ Å⁻³)")
+plot(; xguide="ω (eV)", yguide="SVO DOS (eV⁻¹ Å⁻³)", framestyle=:box)
 plot!(10:η/100:15, dos; label="IAI(), η=$η", legend=:topleft)
 savefig("iai_svo_dos.pdf")
 
@@ -26,6 +26,6 @@ alg = PTR(; npt=npt, rule=AutoBZCore.alloc_rule(h, eltype(bz), bz.syms, npt))
 dos_solver_ptr = IntegralSolver(integrand, bz, alg; abstol=1e-3)
 dos_ptr = hchebinterp(dos_solver_ptr, 10, 15; atol=1e-2)
 
-plot(; xguide="ω (eV)", yguide="SVO DOS (eV⁻¹ Å⁻³)")
+plot(; xguide="ω (eV)", yguide="SVO DOS (eV⁻¹ Å⁻³)", framestyle=:box)
 plot!(10:η/100:15, dos_ptr; label="PTR(; npt=$npt), η=$η", legend=:topleft)
 savefig("ptr_svo_dos.pdf")
