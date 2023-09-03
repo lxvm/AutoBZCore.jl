ff2d44c77e550b0589520304f08a1161db4a0f49
lxvm <lorenzo@vanmunoz.com>
2023-09-03 08:40:33 -0400

    update aps_example

--- aps_example/aps_example.jl
+++ aps_example/aps_example.jl
@@ -1,31 +1,55 @@
 import Pkg
 Pkg.activate(".")           # reproducible environment included
 Pkg.instantiate()           # install dependencies
-include("autobz_extras.jl") # extra functions from AutoBZ.jl
 
-h, bz = load_wannier90_data("svo")
+using WannierIO
 
-bz = cubic_sym_ibz(bz)      # construct ibz for cubic system
+hrdat = read_w90_hrdat("svo_hr.dat")
 
-using AutoBZCore, HChebInterp, Plots, LinearAlgebra
+Rmin, Rmax = extrema(hrdat.Rvectors)
+Rsize = Tuple(Rmax .- Rmin .+ 1)
+n, m = size(first(hrdat.H))
+
+using StaticArrays, OffsetArrays
+
+H_R = OffsetArray(
+    Array{SMatrix{n,m,eltype(eltype(hrdat.H)),n*m}}(undef, Rsize...),
+    Rmin[1]:Rmax[1], Rmin[2]:Rmax[2], Rmin[3]:Rmax[3]
+)
+for (i, h) in zip(hrdat.Rvectors, hrdat.H)
+    H_R[CartesianIndex(Tuple(i))] = h
+end
+
+using AutoBZCore, WannierIO, HChebInterp, LinearAlgebra
+
+bz = load_bz(CubicSymIBZ(), "svo.wout")
+# bz = load_bz(IBZ(), "svo.wout") # works with SymmetryReduceBZ.jl installed
+h = FourierSeries(H_R, period=1.0)
 
 η = 1e-2                    # 10 meV (scattering amplitude)
-dos_integrand(h_k, η, ω) = -imag(tr(inv(complex(ω, η)*I - h_k)))/pi
+dos_integrand(h_k::FourierValue, η, ω) = -imag(tr(inv((ω+im*η)*I - h_k.s)))/pi
 integrand = FourierIntegrand(dos_integrand, h, η)
 
-alg = IAI(; segbufs=AutoBZCore.alloc_segbufs(Float64,Float64,Float64,3))
-dos_solver = IntegralSolver(integrand, bz, alg; abstol=1e-3)
-dos = hchebinterp(dos_solver, 10, 15; atol=1e-2)
+alg = IAI()
+dos_solver_iai = IntegralSolver(integrand, bz, alg; abstol=1e-3)
+dos_iai = hchebinterp(dos_solver_iai, 10, 15; atol=1e-2)
 
-plot(; xguide="ω (eV)", yguide="SVO DOS (eV⁻¹ Å⁻³)", framestyle=:box)
-plot!(10:η/100:15, dos; label="IAI(), η=$η", legend=:topleft)
-savefig("iai_svo_dos.pdf")
-
-npt = 100
-alg = PTR(; npt=npt, rule=AutoBZCore.alloc_rule(h, eltype(bz), bz.syms, npt))
+alg = PTR(npt=100)
 dos_solver_ptr = IntegralSolver(integrand, bz, alg; abstol=1e-3)
 dos_ptr = hchebinterp(dos_solver_ptr, 10, 15; atol=1e-2)
 
-plot(; xguide="ω (eV)", yguide="SVO DOS (eV⁻¹ Å⁻³)", framestyle=:box)
-plot!(10:η/100:15, dos_ptr; label="PTR(; npt=$npt), η=$η", legend=:topleft)
-savefig("ptr_svo_dos.pdf")
+using CairoMakie
+
+set_theme!(fontsize=24, linewidth=4)
+
+fig1 = Figure()
+ax1 = Axis(fig1[1,1], limits=((10,15), (0,det(bz.B)*6)), xlabel="ω (eV)", ylabel="SVO DOS (eV⁻¹ Å⁻³)")
+p1 = lines!(ax1, 10:η/100:15, dos_iai; label="IAI(), η=$η")
+axislegend(ax1)
+save("iai_svo_dos.pdf", fig1)
+
+fig2 = Figure()
+ax2 = Axis(fig2[1,1], limits=((10,15), (0,det(bz.B)*6)), xlabel="ω (eV)", ylabel="SVO DOS (eV⁻¹ Å⁻³)")
+p2 = lines!(ax2, 10:η/100:15, dos_ptr; label="PTR(), η=$η")
+axislegend(ax2)
+save("ptr_svo_dos.pdf", fig2)
