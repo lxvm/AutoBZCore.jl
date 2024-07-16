fed82239f0d00e3c31b71e2fcb9c453cdb52415e
lxvm <lorenzo@vanmunoz.com>
2024-07-16 07:49:34 -0400

    fix undefined export

--- src/AutoBZCore.jl
+++ src/AutoBZCore.jl
@@ -77,7 +77,7 @@ export AuxQuadGKJL, ContQuadGKJL, MeroQuadGKJL
 include("algorithms_iterated.jl")
 export MonkhorstPack, AutoSymPTRJL
 include("algorithms_autosymptr.jl")
-export NestedQuad, AbsoluteEstimate, EvalCounter
+export NestedQuad#, AbsoluteEstimate, EvalCounter
 include("algorithms_meta.jl")
 
 export SymmetricBZ, nsyms
