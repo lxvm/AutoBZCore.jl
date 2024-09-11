6a6d9007421c4dacd68321c487204922d8ccec31
lxvm <lorenzo@vanmunoz.com>
2024-09-11 12:41:01 -0400

    add tests for WannierIOExt

--- test/wannierioext.jl
+++ test/wannierioext.jl
@@ -2,7 +2,16 @@ using Test
 using WannierIO
 using AutoBZCore
 using SymmetryReduceBZ
+using LinearAlgebra: det
 
 # TODO use artefacts to provide an input wout file
-fbz = load_bz(FBZ(), "svo.wout")
-ibz = load_bz(IBZ(), "svo.wout")
+path = joinpath(dirname(dirname(pathof(AutoBZCore))), "aps_example/svo.wout")
+fbz = load_bz(FBZ(), path)
+ibz = load_bz(IBZ(), path)
+@test det(fbz.B) ≈ det(ibz.B)
+fprob = AutoBZCore.AutoBZProblem((x,p) -> 1.0, fbz)
+iprob = AutoBZCore.AutoBZProblem(TrivialRep(), IntegralFunction((x,p) -> 1.0), ibz)
+for alg in (IAI(), PTR())
+    @test det(fbz.B) ≈ AutoBZCore.solve(fprob, alg).value
+    @test det(ibz.B) ≈ AutoBZCore.solve(iprob, alg).value
+end
